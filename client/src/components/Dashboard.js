1600c196ab5a2e950b5f8dc4533b61ea421a651c
MenaMema <amenabarrera@gmail.com>
2023-02-07 17:01:49 +0100

    Dashboard.js page

--- client/src/components/Dashboard.js
+++ client/src/components/Dashboard.js
@@ -1,21 +1,45 @@
 import React from "react";
 import Box from "@mui/material/Box";
+import BoxCat from "./BoxCat";
+import Paper from "@material-ui/core/Paper";
 
 const Dashboard = () => {
   return (
-    <Box
-      sx={{
-        borderRadius: "20px",
-        width: 420,
-        height: 300,
-        backgroundColor: "primary.dark",
-        "&:hover": {
-          backgroundColor: "primary.main",
-          opacity: [0.9, 0.8, 0.7],
-        },
+    <div
+      style={{
+        display: "flex",
+        flexDirection: "column",
+        width: "80%",
+        margin: "auto auto",
+        alignItems: "center",
       }}
-    />
+    >
+      <div style={{ display: "flex", justifyContent: "space-between" }}>
+        <BoxCat text="Comedy" />
+        <BoxCat text="Drama" />
+      </div>
+      <div style={{ display: "flex", justifyContent: "space-between" }}>
+        <BoxCat text="Horror" />
+        <BoxCat text="Cartoons" />
+      </div>
+    </div>
   );
 };
 
 export default Dashboard;
+// export default function BoxSx() {
+//   return (
+//     <Box
+//       sx={{
+//         borderRadius: "20px",
+//         width: 220,
+//         height: 150,
+//         backgroundColor: 'primary.dark',
+//         '&:hover': {
+//           backgroundColor: 'primary.main',
+//           opacity: [0.9, 0.8, 0.7],
+//         },
+//       }}
+//     />
+//   );
+// }
