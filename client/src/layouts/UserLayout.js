6da83c1e3a8cf618e49973f9033cd5248da85c30
Kairos22 <alandiko84@gmail.com>
2023-02-07 13:40:25 +0100

    Add footer

--- client/src/layouts/UserLayout.js
+++ client/src/layouts/UserLayout.js
@@ -2,20 +2,22 @@
 import { Navigate, Outlet } from "react-router-dom";
 import { useContext } from "react";
 import { AppContext } from "../components/Context";
+import Footer from "../components/Footer";
 
 function UserLayout() {
   const { state } = useContext(AppContext);
 
-  if (state.user._id) {
-    return (
-      <>
-        {/* <Header/> */}
-        <Outlet />
-      </>
-    );
-  } else {
-    return <Navigate to="/" />;
-  }
+  // if (state.user._id) {
+  return (
+    <>
+      {/* <Header/> */}
+      <Outlet />
+      <Footer />
+    </>
+  );
+  //   } else {
+  //     return <Navigate to="/" />;
+  //   }
 }
 
 export default UserLayout;
