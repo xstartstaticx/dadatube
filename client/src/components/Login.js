1767dfcc323070b7f0cbe94b7304dd517ae7aa9d
xstartstaticx <daniel.reiller@icloud.com>
2023-02-12 14:39:13 +0100

    changed color theme for login,register,header

--- client/src/components/Login.js
+++ client/src/components/Login.js
@@ -1,8 +1,128 @@
+// import { useContext, useState, useEffect } from "react";
+// import { useNavigate } from "react-router-dom";
+// import { AppContext } from "./Context";
+// import axios from "axios";
+
+// import { CssVarsProvider, useColorScheme } from "@mui/joy/styles";
+// import Sheet from "@mui/joy/Sheet";
+// import Typography from "@mui/joy/Typography";
+// import FormControl from "@mui/joy/FormControl";
+// import FormLabel from "@mui/joy/FormLabel";
+// import Input from "@mui/joy/Input";
+// import Button from "@mui/joy/Button";
+// import Link from "@mui/joy/Link";
+
+// const Login = () => {
+//   const { dispatch, background } = useContext(AppContext);
+//   const navigate = useNavigate();
+
+//   const [data, setData] = useState({
+//     emailOrUsername: "",
+//     password: "",
+//   });
+
+//   console.log("data state in login: ", data);
+
+//   const handleLogin = async () => {
+//     const response = await axios.post("/users/login", data);
+//     console.log("handleLogin response:", response);
+
+//     if (response.data.success) {
+//       dispatch({
+//         type: "login",
+//         payload: response.data.user,
+//       });
+
+//       navigate("/dashboard");
+//     }
+//   };
+//   console.log(background);
+//   return (
+//     <CssVarsProvider>
+//       <main
+//         // className={`border-2 w-screen h-screen  ${
+//         //   background ? "bg-white" : "bg-black"
+//         // }`}
+//         style={{
+//           border: "1px solid red",
+//           width: "100vw",
+//           height: "100vh",
+//           backgroundColor: "#202D33",
+//         }}
+//       >
+//         <Sheet
+//           sx={{
+//             background: "red",
+//             width: 300,
+//             mx: "auto", // margin left & right
+//             my: 4, // margin top & bottom
+//             py: 3, // padding top & bottom
+//             px: 2, // padding left & right
+//             display: "flex",
+//             flexDirection: "column",
+//             gap: 2,
+//             borderRadius: "sm",
+//             boxShadow: "md",
+//           }}
+//           variant="outlined"
+//         >
+//           <div>
+//             <Typography level="h4" component="h1">
+//               <b>Welcome!</b>
+//             </Typography>
+//             <Typography level="body2">Sign in to continue.</Typography>
+//           </div>
+//           <FormControl>
+//             <FormLabel>Who are you?</FormLabel>
+//             <Input
+//               // html input attribute
+//               name="email"
+//               type="email"
+//               placeholder="enter your email or username"
+//               value={data.emailOrUsername}
+//               onChange={(e) =>
+//                 setData({ ...data, emailOrUsername: e.target.value })
+//               }
+//             />
+//           </FormControl>
+//           <FormControl>
+//             <FormLabel>Are you authorized to be here?</FormLabel>
+//             <Input
+//               // html input attribute
+//               name="password"
+//               type="password"
+//               placeholder="enter your password"
+//               value={data.password}
+//               onChange={(e) => setData({ ...data, password: e.target.value })}
+//             />
+//           </FormControl>
+
+//           <Button
+//             type="submit"
+//             onClick={handleLogin}
+//             sx={{ mt: 1 /* margin top */ }}
+//           >
+//             Log in
+//           </Button>
+//           <Typography
+//             endDecorator={<Link href="/register">Sign up</Link>}
+//             fontSize="sm"
+//             sx={{ alignSelf: "center" }}
+//           >
+//             Don&apos;t have an account?
+//           </Typography>
+//         </Sheet>
+//       </main>
+//     </CssVarsProvider>
+//   );
+// };
+
+// export default Login;
+
 import { useContext, useState, useEffect } from "react";
 import { useNavigate } from "react-router-dom";
 import { AppContext } from "./Context";
 import axios from "axios";
-
 import { CssVarsProvider, useColorScheme } from "@mui/joy/styles";
 import Sheet from "@mui/joy/Sheet";
 import Typography from "@mui/joy/Typography";
@@ -11,40 +131,36 @@ import FormLabel from "@mui/joy/FormLabel";
 import Input from "@mui/joy/Input";
 import Button from "@mui/joy/Button";
 import Link from "@mui/joy/Link";
-
+import Header from "./Header";
 const Login = () => {
   const { dispatch, background } = useContext(AppContext);
   const navigate = useNavigate();
-
   const [data, setData] = useState({
     emailOrUsername: "",
     password: "",
   });
-
   console.log("data state in login: ", data);
-
   const handleLogin = async () => {
     const response = await axios.post("/users/login", data);
     console.log("handleLogin response:", response);
-
     if (response.data.success) {
       dispatch({
         type: "login",
         payload: response.data.user,
       });
-
       navigate("/dashboard");
     }
   };
   console.log(background);
   return (
     <CssVarsProvider>
+      {/* <Header /> */}
       <main
         // className={`border-2 w-screen h-screen  ${
         //   background ? "bg-white" : "bg-black"
         // }`}
         style={{
-          border: "1px solid red",
+          border: "1px solid #202D33",
           width: "100vw",
           height: "100vh",
           backgroundColor: "#202D33",
@@ -52,17 +168,17 @@ const Login = () => {
       >
         <Sheet
           sx={{
-            background: "red",
-            width: 300,
+            width: 600,
             mx: "auto", // margin left & right
-            my: 4, // margin top & bottom
-            py: 3, // padding top & bottom
-            px: 2, // padding left & right
+            my: 20, // margin top & bottom
+            py: 6, // padding top & bottom
+            px: 6, // padding left & right
             display: "flex",
             flexDirection: "column",
             gap: 2,
             borderRadius: "sm",
             boxShadow: "md",
+            backgroundColor: "#70BBB1",
           }}
           variant="outlined"
         >
@@ -96,7 +212,6 @@ const Login = () => {
               onChange={(e) => setData({ ...data, password: e.target.value })}
             />
           </FormControl>
-
           <Button
             type="submit"
             onClick={handleLogin}
@@ -116,5 +231,4 @@ const Login = () => {
     </CssVarsProvider>
   );
 };
-
 export default Login;
--- client/src/components/Register.js
+++ client/src/components/Register.js
@@ -1,8 +1,141 @@
+// import { useState, useEffect, useContext } from "react";
+// import { useNavigate } from "react-router-dom";
+// import { AppContext } from "./Context";
+// import axios from "axios";
+
+// import { CssVarsProvider, useColorScheme } from "@mui/joy/styles";
+// import Sheet from "@mui/joy/Sheet";
+// import Typography from "@mui/joy/Typography";
+// import FormControl from "@mui/joy/FormControl";
+// import FormLabel from "@mui/joy/FormLabel";
+// import Input from "@mui/joy/Input";
+// import Button from "@mui/joy/Button";
+// import Link from "@mui/joy/Link";
+
+// const Register = () => {
+//   const [data, setData] = useState({
+//     username: "",
+//     email: "",
+//     password: "",
+//   });
+
+//   const { dispatch, background } = useContext(AppContext);
+
+//   const navigate = useNavigate();
+
+//   const handleRegister = async () => {
+//     try {
+//       const response = await axios.post("/users/register", data);
+//       console.log("handleRegister ~ response:", response);
+
+//       if (response.data.success) {
+//         navigate("/");
+//       }
+//       //   else {
+//       //     if (response.data.errorId === 2)
+//       //       alert("Username must be more than 2 characters");
+//       //   }
+//     } catch (error) {
+//       console.log("error:", error.message);
+//     }
+//   };
+
+//   return (
+//     <CssVarsProvider>
+//       <main
+//         // className={`border-2 w-screen h-screen  ${
+//         //   background ? "bg-white" : "bg-black"
+//         // }`}
+//         style={{
+//           border: "1px solid red",
+//           width: "100vw",
+//           height: "100vh",
+//           backgroundColor: "#202D33",
+//         }}
+//       >
+//         <Sheet
+//           sx={{
+//             width: 300,
+//             mx: "auto", // margin left & right
+//             my: 4, // margin top & bottom
+//             py: 3, // padding top & bottom
+//             px: 2, // padding left & right
+//             display: "flex",
+//             flexDirection: "column",
+//             gap: 2,
+//             borderRadius: "sm",
+//             boxShadow: "md",
+//           }}
+//           variant="outlined"
+//         >
+//           <div>
+//             <Typography level="h4" component="h1">
+//               <b>Welcome!</b>
+//             </Typography>
+//             <Typography level="body2">
+//               Please register a new account.
+//             </Typography>
+//           </div>
+//           <FormControl>
+//             <FormLabel>Create a unique username:</FormLabel>
+//             <Input
+//               // html input attribute
+//               name="username"
+//               type="text"
+//               placeholder="enter your username"
+//               value={data.username}
+//               onChange={(e) => setData({ ...data, username: e.target.value })}
+//             />
+//           </FormControl>
+//           <FormControl>
+//             <FormLabel>What's your email?</FormLabel>
+//             <Input
+//               // html input attribute
+//               name="email"
+//               type="email"
+//               placeholder="enter your email"
+//               value={data.email}
+//               onChange={(e) => setData({ ...data, email: e.target.value })}
+//             />
+//           </FormControl>
+//           <FormControl>
+//             <FormLabel>Create a good password:</FormLabel>
+//             <Input
+//               // html input attribute
+//               name="password"
+//               type="password"
+//               placeholder="enter your password"
+//               value={data.password}
+//               onChange={(e) => setData({ ...data, password: e.target.value })}
+//             />
+//           </FormControl>
+
+//           <Button
+//             type="submit"
+//             onClick={handleRegister}
+//             sx={{ mt: 1 /* margin top */ }}
+//           >
+//             Sign me up!
+//           </Button>
+//           <Typography
+//             endDecorator={<Link href="/">Login</Link>}
+//             fontSize="sm"
+//             sx={{ alignSelf: "center" }}
+//           >
+//             Have an account already?
+//           </Typography>
+//         </Sheet>
+//       </main>
+//     </CssVarsProvider>
+//   );
+// };
+
+// export default Register;
+
 import { useState, useEffect, useContext } from "react";
 import { useNavigate } from "react-router-dom";
 import { AppContext } from "./Context";
 import axios from "axios";
-
 import { CssVarsProvider, useColorScheme } from "@mui/joy/styles";
 import Sheet from "@mui/joy/Sheet";
 import Typography from "@mui/joy/Typography";
@@ -11,23 +144,19 @@ import FormLabel from "@mui/joy/FormLabel";
 import Input from "@mui/joy/Input";
 import Button from "@mui/joy/Button";
 import Link from "@mui/joy/Link";
-
+import Header from "./Header";
 const Register = () => {
   const [data, setData] = useState({
     username: "",
     email: "",
     password: "",
   });
-
   const { dispatch, background } = useContext(AppContext);
-
   const navigate = useNavigate();
-
   const handleRegister = async () => {
     try {
       const response = await axios.post("/users/register", data);
       console.log("handleRegister ~ response:", response);
-
       if (response.data.success) {
         navigate("/");
       }
@@ -39,15 +168,15 @@ const Register = () => {
       console.log("error:", error.message);
     }
   };
-
   return (
     <CssVarsProvider>
+      {/* <Header /> */}
       <main
         // className={`border-2 w-screen h-screen  ${
         //   background ? "bg-white" : "bg-black"
         // }`}
         style={{
-          border: "1px solid red",
+          border: "1px solid #202D33",
           width: "100vw",
           height: "100vh",
           backgroundColor: "#202D33",
@@ -55,16 +184,17 @@ const Register = () => {
       >
         <Sheet
           sx={{
-            width: 300,
+            width: 600,
             mx: "auto", // margin left & right
-            my: 4, // margin top & bottom
-            py: 3, // padding top & bottom
-            px: 2, // padding left & right
+            my: 20, // margin top & bottom
+            py: 6, // padding top & bottom
+            px: 6, // padding left & right
             display: "flex",
             flexDirection: "column",
             gap: 2,
             borderRadius: "sm",
             boxShadow: "md",
+            backgroundColor: "#70BBB1",
           }}
           variant="outlined"
         >
@@ -109,7 +239,6 @@ const Register = () => {
               onChange={(e) => setData({ ...data, password: e.target.value })}
             />
           </FormControl>
-
           <Button
             type="submit"
             onClick={handleRegister}
@@ -129,5 +258,4 @@ const Register = () => {
     </CssVarsProvider>
   );
 };
-
 export default Register;
